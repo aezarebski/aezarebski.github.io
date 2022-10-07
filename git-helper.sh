226f143298fd4b185f0bd10853b4f362a06176a0
Alex Zarebski <aezarebski@gmail.com>
2022-10-07 12:03:07 +0100

    update website friday 12:03 pm

--- git-helper.sh
+++ git-helper.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 # A bunch of boring lists
 git add lists/books.html
@@ -5,3 +6,15 @@ git add lists/links.html
 git add lists/movies-and-series.html
 git add lists/quotes.html
 git add lists/zarebski.bib
+
+# The ggplot2 gallery
+git add misc/ggplot2/gallery.html
+
+for ix in $(seq -f "%02g" 1 8);
+do
+    git add ./misc/ggplot2/fig$ix.png
+done
+
+# A bunch of notes files
+git add notes/r-notes.html
+git add notes/tutoring-notes.html
