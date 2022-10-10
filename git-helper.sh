a803a8a1b058b0448efc3b09022835b65a7cfd71
Alex Zarebski <aezarebski@gmail.com>
2022-10-10 18:08:22 +0100

    update website monday  6:08 pm

--- git-helper.sh
+++ git-helper.sh
@@ -15,6 +15,14 @@ do
     git add ./misc/ggplot2/fig$ix.png
 done
 
+# The d3 gallery
+git add misc/d3/gallery.html
+
+for ix in $(seq -f "%02g" 1 5);
+do
+    git add ./misc/d3/fig$ix.js
+done
+
 # A bunch of notes files
 git add notes/r-notes.html
 git add notes/tutoring-notes.html
--- misc/d3/fig01.js
+++ misc/d3/fig01.js
@@ -1,9 +1,17 @@
 function makeFig01() {
-    d3.csv("iris.csv").then((data) => {
+    var margin = { top: 10, right: 30, bottom: 50, left: 60 };
+    var width = 460 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
 
-        var margin = { top: 10, right: 30, bottom: 50, left: 60 };
-        var width = 460 - margin.left - margin.right;
-        var height = 400 - margin.top - margin.bottom;
+    var fig = d3.select("#fig01")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("iris.csv").then((data) => {
 
         var x = d3.scaleLinear()
             .domain(d3.extent(data, d => Number(d.sepalLength)))
@@ -16,14 +24,6 @@ function makeFig01() {
         var colourScale = d3.scaleOrdinal()
             .domain(data.map(d => d.species))
             .range(d3.schemeCategory10);
-
-        var fig = d3.select("#fig01")
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform",
-                  "translate(" + margin.left + "," + margin.top + ")");
 
         fig.append("g")
             .attr("transform", "translate(" + 0 + "," + height + ")")
@@ -52,5 +52,51 @@ function makeFig01() {
             .attr("x", width/2 + margin.left)
             .attr("y", height + margin.top + margin.bottom/2)
             .text("Sepal Length");
+
+        // The following annotation code needs to be done in here to prevent the
+        // function returning too early.
+
+        // We set up an object to compute a natural curve connecting the
+        // specified points and then append this as a path element so it is
+        // drawn.
+        const curve = d3.line().curve(d3.curveNatural);
+        const points = [
+            [x(5.0), y(2.0)],
+            [x(0.5*(6.5+5.0)-0.2), y(2.5+0.6)],
+            [x(6.5), y(3.0)]
+        ];
+        fig.append("path")
+            .attr("d", curve(points))
+            .attr("stroke", "darkgreen")
+            .style("stroke-width", "2px")
+            .attr("fill", "none");
+
+        // The text annotation is a bit boring, but this demonstrates how you
+        // might go about making it a bit more interesting and tweaking the
+        // appearance.
+        var yRect = 3.5;
+
+        fig.append("a")
+            .attr("xlink:href", "https://en.wikipedia.org/wiki/Doge_(meme)")
+            .append("rect")
+            .attr("x", x(6.5))
+            .attr("y", y(yRect))
+            .attr("width", x(8.1) - x(6.5))
+            .attr("height", y(yRect) - y(yRect + 1.0))
+            .style("fill", "lightgreen")
+            .attr("rx", 10)
+            .attr("ry", 10)
+            .attr("stroke", "darkgreen")
+            .attr("stroke-width", "2px");
+
+        fig.append("text")
+            .attr("text-anchor", "start")
+            .attr("x", x(6.5 + 0.1))
+            .attr("y", y(3.0 - 0.1))
+            .style("fill", "darkgreen")
+            .style("font-size", "18px")
+            .style("font-style", "italic")
+            .text("Wow! Click Me!");
+
     });
 };
